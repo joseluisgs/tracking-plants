cf084088f32f1df59655add778c874ccffa6d4df
joseluisgs <joseluisgonsan@gmail.com>
2020-11-14 17:31:52 +0100

    Gestión de eventos

--- src/components/ui/Autocomplete.vue
+++ src/components/ui/Autocomplete.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="autocomplete-wraper">
-   <div>
-     <input type="text" class="border py-1 px-1 rounded w-full focus:outline-none">
-   </div>
-   <div></div>
+    <div>
+      <input
+        type="text"
+        class="border py-1 px-1 rounded w-full focus:outline-none"
+        v-model="itemSelected"
+        @input="changeInputValue"
+      />
+    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Autocomplete',
+  // Mis datos del modelo
+  data: () => ({
+    itemSelected: '',
+  }),
+  // Mis métodos
+  methods: {
+    changeInputValue() {
+      // Para evitar muchas búsquedas lo hacemos con el valor mayor a 3
+      if (this.itemSelected.length >= 3) {
+      // Emitimos el evento de buscar que se llamará inpu y devolverá el valor event
+        this.$emit('input', this.itemSelected);
+        // console.log(`Autocomplete Value: ${this.itemSelected}`);
+      }
+    },
+  },
 };
 </script>
 
--- src/views/add/index.vue
+++ src/views/add/index.vue
@@ -2,7 +2,10 @@
   <section>
     <h1 class="font-bold text-2xl">Nueva Planta</h1>
     <form class="mt-8">
-      <Autocomplete />
+      <!-- Recibe el evento llamado input de autocomplete -->
+      <Autocomplete
+        @input="onChangeAutocomplete"
+      />
       <div class="flex justify-center mt-8">
         <button class="border rounded py-2 px-6 bg-green-600 text-white font-bold">Guardar</button>
       </div>
@@ -17,6 +20,13 @@ export default {
   name: 'AddView',
   components: {
     Autocomplete,
+  },
+  // Mis métodos
+  methods: {
+    onChangeAutocomplete(value) {
+      // Llamamos a la api
+      console.log(value);
+    },
   },
 };
 </script>
