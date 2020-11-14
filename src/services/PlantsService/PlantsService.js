2812d0498e17c651a3478b60bb9b027a4602debc
joseluisgs <joseluisgonsan@gmail.com>
2020-11-14 18:12:52 +0100

    Plant Service Rename Config

--- src/services/PlantsService/PlantsService.js
+++ src/services/PlantsService/PlantsService.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-const TOKEN = './PlantsServiceToken.js';
+const TOKEN = './PlantsServiceConfig.js';
 const URL_BASE = `https://trefle.io/api/v1/plants?token=${TOKEN}`;
 
 // &filter[common_name]=beach%20strawberry
