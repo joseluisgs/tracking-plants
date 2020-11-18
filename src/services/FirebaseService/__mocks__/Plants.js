bb33603fc20b0463cf6386a1a79b5125d9c9f101
joseluisgs <joseluisgonsan@gmail.com>
2020-11-18 19:11:08 +0100

    Testing Plants Firebase Save con Mocks

---
+++ src/services/FirebaseService/__mocks__/Plants.js
@@ -0,0 +1,8 @@
+export default {
+  // Crea uno nuevo
+  // https://firebase.google.com/docs/firestore/manage-data/add-data?hl=es-419#web
+  async post(payload) {
+    const res = payload;
+    return res;
+  },
+};
