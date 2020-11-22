1a501eee13f0fbd10369400b48eb0489b5bea9c2
joseluisgs <joseluisgonsan@gmail.com>
2020-11-22 11:42:40 +0100

    Vuex store get Details

--- src/store/plants/actions.js
+++ src/store/plants/actions.js
@@ -3,15 +3,23 @@
 // API REST u otro lugar solo cambiaríamos la función
 import PlantService from '@/services/FirebaseService/PlantsFirebase';
 
+// Las acciones son asincronas y llaman a mutaciones siv van a tocar el estado
 export default {
+  // Salvamos una planta
   async savePlant(_, payload) {
-    const res = await PlantService.post(payload);
+    return PlantService.post(payload);
   },
 
+  // Listamos las plantas, commit porque llamamos a una mutación para actualizat el estado
   async listPlants({ commit }) {
     const res = await PlantService.get();
     // Llamamos a la mutación
     commit('setPlants', res);
     return res;
+  },
+
+  // Obtenemos una planta
+  async getPlant(_, plantId) {
+    return PlantService.getBtId(plantId);
   },
 };
